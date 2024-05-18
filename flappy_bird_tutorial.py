c78653a9a7b7afb348271c3f6a8782d8174a17e3
cs-cbonh002 <bonham_36@hotmail.com>
2024-05-18 16:46:39 -0400

    Implement main function and game loop.

--- flappy_bird_tutorial.py
+++ flappy_bird_tutorial.py
@@ -4,7 +4,7 @@ import time
 import os
 import random
 
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 
@@ -82,3 +82,28 @@ class Bird:
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+    bird = Bird(200, 200)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        bird.move()
+        draw_window(win, bird)
+
+    pygame.quit()
+    quit()
+
+main()
